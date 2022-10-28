a96adee7bed46d3d5ebe0b3e22e9104288b38cf8
tuk81273@temple.edu <78782966+Braddinger13@users.noreply.github.com>
2022-10-27 21:58:19 -0400

    Background image appears

--- UserInterface/GameView.java
+++ UserInterface/GameView.java
@@ -1,4 +1,48 @@
 package UserInterface;
 
-public class GameView {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GameView extends JFrame {
+
+
+    JFrame frame = new JFrame();
+    JLabel background = new JLabel();
+
+    private ImageIcon backgroundImage;
+
+    public GameView() {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("UserInterface/sunnyNeighborhood.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //setting image
+        backgroundImage = new ImageIcon(img);
+        background.setIcon(backgroundImage);
+
+
+        //adding label with image to frame
+        frame.add(background);
+
+        frame.setTitle("Lemonade Stand");
+        frame.setSize(600,400);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GameView();
+    }
+
+
+
 }
