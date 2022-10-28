df1f6ed01a36795bd71e05920753ea1671bcd92e
rickwu135 <rickwu135@gmail.com>
2022-10-28 13:03:28 -0400

    added new lines

--- UserInterface/MainMenu.java
+++ UserInterface/MainMenu.java
@@ -65,19 +65,19 @@ public class MainMenu {
                 frame.dispose();
                 JLabel label = new JLabel("How to play");
                 JLabel label1 = new JLabel("""
-                    Weather:
-                    Keep an eye on the forecast. This will help you estimate supplies you will need, how to formulate your lemonade and how much to charge.
+                    Weather:\n
+                    Keep an eye on the forecast. This will help you estimate supplies you will need, how to formulate your lemonade and how much to charge.\n
     
-                    Pricing:
-                    On hotter days, customers will pay more. On colder days, lower your prices to attract customers. You can adjust your price at any time during the day.
+                    Pricing:\n
+                    On hotter days, customers will pay more. On colder days, lower your prices to attract customers. You can adjust your price at any time during the day.\n
                     
-                    Inventory:
-                    Stock up on cups when the price is low. Keep lemons and sugar stocks fairly low as they can go bad. Ice melts at the end of every day, so buy ice only according to your daily needs.
+                    Inventory:\n
+                    Stock up on cups when the price is low. Keep lemons and sugar stocks fairly low as they can go bad. Ice melts at the end of every day, so buy ice only according to your daily needs.\n
                     
-                    Quality:
-                    On colder days, add additional lemons and sugar to your pitchers to entice customers, and reduce ice to save money. On hot days, decrease your lemons and sugar to the default, and increase ice cubes.
+                    Quality:\n
+                    On colder days, add additional lemons and sugar to your pitchers to entice customers, and reduce ice to save money. On hot days, decrease your lemons and sugar to the default, and increase ice cubes.\n
                     
-                    Customers:
+                    Customers:\n
                     Keep an eye on passing customers. Symbols or words appear over their heads indicating whether they like or dislike your lemonade, or if they think your prices are fair. Always try to please the customers.""");
                 JFrame frame = new JFrame();
                 JPanel panel = new JPanel();
