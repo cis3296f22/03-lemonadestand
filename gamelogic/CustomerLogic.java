a7449d9e5a7bc5dc71b7e49573ba64f79e8515b3
Ricky <rickwu135@gmail.com>
2022-11-15 15:52:26 -0500

    ???

---
+++ gamelogic/CustomerLogic.java
@@ -0,0 +1,25 @@
+/*import java.util.Random;
+public class CustomerLogic {
+    Random random = new Random();
+
+    WeatherForecast obj = new WeatherForecast();
+    int customers2 = obj.customers;
+
+    //Customer Logic for amount
+    if(pricePerCup >2){
+        customers2 = customers2 - (customers2 * 10 / 100);//reduce customers by 10%
+    }
+    else if(pricePerCup< 0.50){
+        customers2 = customers2 + (customers2 * 10 / 100);//Increase Customers by 10%
+    }
+    if(lemonsPerCup >4||lemonsPerCup< 2){
+        customers2 = customers2 - (customers2 * 10 / 100);
+    }
+    if(sugarPerCup >4||sugarPerCup< 2){
+        customers2 = customers2 - (customers2 * 10 / 100);
+    }
+    if(icePerCup >3||icePerCup< 1){
+        customers2 = customers2 - (customers2 * 10 / 100);
+    }
+
+}*/
