175cb5fffef98640f180aa427d94404bb2a67bf9
Steven <slayer38@ymail.com>
2022-11-17 23:58:58 -0500

    Put it in the UserInterface

--- UserInterface/LemonadeStandModel.java
+++ UserInterface/LemonadeStandModel.java
@@ -218,5 +218,47 @@ public class LemonadeStandModel {
         return canSubtractIce() && canSubtractCups() && canSubtractLemons() && canSubtractSugar();
     }
 
+    public int[] CustomerLogic(){
+        WeatherForecast obj = new WeatherForecast();
+        //int customers[0] = obj.getTemperature();
+        int[] customers = new int[5];
+        customers[0] = obj.getTemperature();
 
+        int temperature = obj.temperature;
+
+
+        System.out.println("LemonadeStand Temperature " + temperature);
+        System.out.println("Customers2 unmodified "+ customers[0]);//Test for modifying
+
+        if (pricePer > 2) {
+            customers[0] = customers[0] - (customers[0] * 10 / 100);//reduce customers by 10%
+            customers[1] = customers[1] + (customers[0] * 10 / 100);
+        }
+        else if (pricePer < 0.50) {
+            customers[0] = customers[0] + (customers[0] * 10 / 100);//Increase Customers by 10%
+
+        }
+        if (lemonsPer > 3 || lemonsPer < 2) {
+            customers[0] = customers[0] - (customers[0] * 10 / 100);
+            customers[2] = customers[1] + (customers[0] * 10 / 100);
+        }
+        if (sugarPer > 4 || sugarPer < 2) {
+            customers[0] = customers[0] - (customers[0] * 10 / 100);
+            customers[3] = customers[1] + (customers[0] * 10 / 100);
+        }
+        if(temperature > 80)
+            if (icePer > 3) {
+                customers[0] = customers[0] + (customers[0] * 10 / 100);
+            }
+            else if(icePer == 0){
+                customers[0] = customers[0] - (customers[0] * 10 / 100);
+                customers[4] = customers[1] + (customers[0] * 10 / 100);
+            }
+            else if(temperature <= 80)
+                if (icePer == 0){
+                    customers[0] = customers[0] - (customers[0] * 10 / 100);
+                    customers[4] = customers[1] + (customers[0] * 10 / 100);
+                }
+        return customers;
+    }
 }
