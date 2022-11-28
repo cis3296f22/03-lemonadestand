07584b6b6f073cd9bb5172d485bac5c3dd1141b5
Ricky <rickwu135@gmail.com>
2022-11-28 17:41:12 -0500

    customers/weather for gameview. - Steven

--- src/gamelogic/WeatherForecast.java
+++ src/gamelogic/WeatherForecast.java
@@ -8,39 +8,63 @@ public class WeatherForecast {
         int min = 30;
         int max = 100;
         int temperature = (int) (Math.random() * (max - min + 1) + min);
-        int rain = (int) (Math.random() * (2 - 1 + 1) + 1);
+        int rain = (int) (Math.random() * (3 - 1 + 1) + 1);
         int customers;
+        String weather = getWeather();
 
 
         public int getTemperature(){
             //System.out.println("WeatherForecast temperature " +temperature);
             if (temperature <= 30) {
                 if (rain == 1) {
-                    System.out.println("It's snowing");
+                    //System.out.println("It's snowing");
                     customers = 10 - random.nextInt(10);
-                } else {
-                    System.out.println("clear weather");
+                } else if(rain ==2 ) {
+                    //System.out.println("clear weather");
                     customers = 20 - random.nextInt(10);
+                }
+                else{
+                    //System.out.println("Cloudy");
+                    customers = 15 - random.nextInt(10);
                 }
                 return customers;
             } else if ((30 < temperature) || (temperature < 60)) {
                 if (rain == 1) {
-                    System.out.println("It's raining");
-                    customers = 30 - random.nextInt(10);
-                } else {
-                    System.out.println("clear weather");
+                    //System.out.println("It's raining");
+                    customers = 30 - random.nextInt(20);
+                } else if(rain ==2 ) {
+                    //System.out.println("clear weather");
                     customers = 40 - random.nextInt(10);
+                }
+                else{
+                    //System.out.println("Cloudy");
+                    customers = 35 - random.nextInt(15);
                 }
                 return customers;
             } else {
                 if (rain == 1) {
                     System.out.println("It's raining");
                     customers = 50 - random.nextInt(10);
-                } else {
+                } else if(rain ==2 ) {
                     System.out.println("clear weather");
                     customers = 60 + random.nextInt(20);
                 }
+                else{
+                    //System.out.println("Cloudy");
+                    customers = 55 - random.nextInt(15);
+                }
                 return customers;
             }
+        }
+        public String getWeather(){
+            if (rain == 1) {
+                return ("Rainy");
+            } else if(rain ==2 ) {
+                return "Sunny";
+            }
+            else{
+                return "Cloudy";
+            }
+
         }
     }
